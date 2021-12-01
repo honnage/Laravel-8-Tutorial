64a68696e2601c0a74cdba18290db549e32d0e44
honnage <hon_da-1998@hotmail.com>
2021-12-01 11:06:12 +0700

    Basic Routing

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users/{name?}', function ($name = null) {
+    return 'hi '.$name;
+});
+
+Route::get('products/{id?}', function ($id = null){
+    return 'Product id is '.$id;
+});
+
+Route::match(['get', 'post'], '/students', function (Request $req) {
+    return 'Requested method is '.$req->method();
+});
+
+Route::any('posts', function (Request $req) {
+    return 'Requested method is '.$req->method();
+});
